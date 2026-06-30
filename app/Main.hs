661c725ef89c0aa538c837cd2f499ec6bb6cf382
TigRig <tigrig.crex@gmail.com>
2026-06-30 20:56:12 +0900

    haskell-todo list 仮実装

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import System.Environment (getArgs)
+import System.IO (hSetEncoding, stdout, stderr, utf8)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  hSetEncoding stdout utf8
+  hSetEncoding stderr utf8
+
+  args <- getArgs
+  case args of
+    ["list"] -> putStrLn "list が実行されました"
+    _ -> putStrLn "不正な引数です。'list' を指定してください。"
