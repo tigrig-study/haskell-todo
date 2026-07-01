e26b6be073dc5fe3624032e6081acbb73caa82a4
TigRig <tigrig.crex@gmail.com>
2026-07-01 13:50:43 +0900

    list 実装

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import System.Environment (getArgs)
-import System.IO (withFile, IOMode(AppendMode), hSetEncoding, utf8, hPutStrLn)
+import System.IO (IOMode (AppendMode, ReadMode), hGetContents, hPutStrLn, hSetEncoding, utf8, withFile)
 
 main :: IO ()
 main = do
@@ -12,5 +12,13 @@ main = do
         hSetEncoding handle utf8
         hPutStrLn handle task
       putStrLn $ "タスクが追加されました: " ++ task
-    ["list"] -> putStrLn "list が実行されました"
+    ["list"] -> do
+      withFile "todo.txt" ReadMode $ \handle -> do
+        hSetEncoding handle utf8
+        contents <- hGetContents handle
+        let tasks = formatTasks $ lines contents
+        mapM_ putStrLn tasks
     _ -> putStrLn "不正な引数です。'list' を指定してください。"
+
+formatTasks :: [String] -> [String]
+formatTasks = zipWith (\i task -> show i ++ ". " ++ task) [1 :: Integer ..]
