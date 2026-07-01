20724ce60031a81e63750b9f83a3386ee32f8072
TigRig <tigrig.crex@gmail.com>
2026-07-01 11:26:14 +0900

    add 実装

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,16 @@
 module Main where
 
 import System.Environment (getArgs)
-
--- 1.  ["add", task]  のようなパターンマッチを追加し、一時的に  putStrLn で「追加しようとしているタスクの内容」をターミナルに表示できるか確認する。
--- 2. 上手く受け取れたら、それを  System.IO  の関数を使ってファイル（ todo.txt など）へ追記する処理に置き換える。
--- 3. 追加が成功した旨のメッセージをターミナルに表示する。
+import System.IO (withFile, IOMode(AppendMode), hSetEncoding, utf8, hPutStrLn)
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    ["add", task] -> putStrLn task
+    ["add", task] -> do
+      withFile "todo.txt" AppendMode $ \handle -> do
+        hSetEncoding handle utf8
+        hPutStrLn handle task
+      putStrLn $ "タスクが追加されました: " ++ task
     ["list"] -> putStrLn "list が実行されました"
     _ -> putStrLn "不正な引数です。'list' を指定してください。"
